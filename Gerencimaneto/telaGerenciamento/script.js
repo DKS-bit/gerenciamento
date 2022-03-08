5a0dcd01331840d64335b82a0cee746c8096d897
DKS-bit <85642822+DKS-bit@users.noreply.github.com>
2022-03-07 22:38:45 -0300

    Add files via upload

---
+++ Gerencimaneto/telaGerenciamento/script.js
@@ -0,0 +1,10 @@
+window.onload = function(){ 
+    document.getElementById('incluir').onclick= incluirCliente;
+
+
+function incluirCliente(){
+
+}
+};
+//Plano eh trabalhar com um vetor de clientes utilizando json e ajax com o banco de dados
+//que irar dar load ao entrar na pagina, remover, ou adicionar um cliente.
